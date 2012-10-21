853dec5ef0f59fc7d6a3ffd85ab8f58b68e4c712
Tom Lyons <tbl3rd@gmail.com>
2012-10-20 22:59:35 -0400

    Use more Java to simplify.

--- src/csvfromvcf/base64.clj
+++ src/csvfromvcf/base64.clj
@@ -4,13 +4,13 @@
   "Return the string base64-decoded from s."
   [s]
   (let [ks "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-        c2i (into {} (map (fn [k v] [k v]) ks (range 0 (count ks))))
-        unpack4
-        (fn [b cs]
-          (if (first cs)
-            (recur (bit-or (bit-shift-left b 6) (c2i (first cs))) (next cs))
-            (.toByteArray (BigInteger/valueOf b))))]
-    (apply str (map #(apply str (map char (unpack4 0 %))) (partition 4 4 "" s)))))
+        c2i (into {} (map (fn [k v] [k v]) ks (range 0 (count ks))))]
+    (apply str
+           (map char (.toByteArray
+                      (reduce
+                       (fn [bi c]
+                         (.or (.shiftLeft bi 6) (BigInteger/valueOf (c2i c))))
+                       (BigInteger/valueOf 0) s))))))
 
 ;; 2.00 oz spiced rum (Captain Morgan's)
 ;; 1.00 oz Cointreau
