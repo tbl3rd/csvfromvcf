de4468fc42b969388aea361296963f3e77101260
Tom Lyons <tbl3rd@gmail.com>
2012-10-24 22:05:20 -0400

    Handle base64 = padding properly on decode.

--- src/csvfromvcf/base64.clj
+++ src/csvfromvcf/base64.clj
@@ -6,22 +6,47 @@
   (let [ks "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"]
     (zipmap ks (range 0 (count ks)))))
 
-(defn base64-decode
-  "Return the string base64-decoded from s."
+(defn quad
+  "Convert sequence s into a sequence of sequences of up to 4 characters."
   [s]
-  (apply str
-         (map char
-              (.toByteArray
-               (reduce (fn [bi c]
-                         (.or (.shiftLeft bi 6) (BigInteger/valueOf (c2i c))))
-                       (BigInteger/valueOf 0) s)))))
+  (partition-all 4 (filter c2i s)))
+
+(defn decode-chunk
+  "Decode a chunk of 2 to 4 base64 characters."
+  [chunk]
+  (let [n (count chunk)
+        i (reduce (fn [i c] (bit-or (bit-shift-left i 6) (c2i c))) 0 chunk)]
+    (cond (= n 4)
+          [(bit-and 0xff (bit-shift-right i 16))
+           (bit-and 0xff (bit-shift-right i  8))
+           (bit-and 0xff (bit-shift-right i  0))]
+          (= n 3)
+          [(bit-and 0xff (bit-shift-right i 10))
+           (bit-and 0xff (bit-shift-right i  2))]
+          (= n 2)
+          [(bit-and 0xff (bit-shift-right i  4))])))
+
+(defn base64->bytes
+  "Decode the sequence s of base64 characters into a byte sequence."
+  [s]
+  (map byte (flatten (map decode-chunk (quad s)))))
+
+(defn base64->string
+  "Decode the sequence s of base64 characters into a string."
+  [s]
+  (apply str (map char (flatten (map decode-chunk (quad s))))))
 
 ;; 2.00 oz spiced rum (Captain Morgan's)
 ;; 1.00 oz Cointreau
 ;; 0.33 oz lemon juice
 ;; dust with cinnamon
 ;; and garnish with lemon twist
-(def test
+(def recipe
   "Mi4wMCBveiBzcGljZWQgcnVtIChDYXB0YWluIE1vcmdhbidzKQ0xLjAwIG96IENvaW50cmVhdQ0wLjMzIG96IGxlbW9uIGp1aWNlDWR1c3Qgd2l0aCBjaW5uYW1vbg1hbmQgZ2FybmlzaCB3aXRoIGxlbW9uIHR3aXN0")
 
-;; (base64-decode test)
+(def n "SmFtZXMgJiBLYXJlbiA=")
+
+;; (base64->string recipe)
+;; (base64->string n)
+;; (base64-decode recipe)
+;; (base64-decode n)
