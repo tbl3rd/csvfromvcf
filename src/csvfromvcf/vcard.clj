0977143f73d513ac8795e28d2a7ba555ac144207
Tom Lyons <tbl3rd@gmail.com>
2012-10-27 21:13:34 -0400

    Write out CSV, but missing some of multi-line addresses.

--- src/csvfromvcf/vcard.clj
+++ src/csvfromvcf/vcard.clj
@@ -2,7 +2,7 @@
   (:require [clojure.java.io])
   (:use [csvfromvcf.base64]))
 
-(defn linify
+(defn- linify
   "Join multiple lines from lines on CRLFSP sequences."
   ([lines] (linify lines ""))
   ([lines sofar]
@@ -16,7 +16,7 @@
               (linify (next lines) (str sofar trimmed)))))
         (cons sofar ())))))
 
-(defn parsify
+(defn- parsify
   "Split line at leftmost : and then at ;s returning a vector of
   vectors.  The first vector is of property name and parameters.
   The second vector is of property values."
@@ -25,7 +25,7 @@
         (map #(clojure.string/split % #";")
              (clojure.string/split line #":" 2))))
 
-(defn classify
+(defn- classify
   "Classify the vCard properties props according to contact field."
   [props]
   (let [tag (first props)]
@@ -45,49 +45,69 @@
           (cond (some #{"CELL"} props) :mobile
                 (some #{"HOME"} props) :home
                 (some #{"WORK"} props) :work
+                (some #{"VOICE"} props) :voice
                 :else :ignore)
           :else :ignore)))
 
-(def base64? #{:note})
+(def ^{:doc "True if the tag's value is encoded in base64."
+       :private true}
+  base64?
+  #{:note})
 
-(defn pairify
+(defn- pairify
   "Turn line into a pair vector with PropertyName key and
   PropertyValue value."
   [line]
   (let [[parameters values] (parsify line)
         k (classify parameters)
-        decode (if (base64? k) base64-decode identity)]
+        decode (if (base64? k) base64->string identity)]
     [k (clojure.string/join
         "\n"
         (map decode (remove clojure.string/blank? values)))]))
 
-(defn mapify
+(defn- mapify
   "Return a map of PropertyName to vector of PropertyValues
   representing the vCard file cf."
   [cf]
-  (println "mapify" cf)
   (with-open [in (clojure.java.io/reader cf)]
     (reduce (fn [m [k v]] (assoc m k (conj (m k []) v)))
             {} (map pairify (linify (line-seq in))))))
 
-(defn cardify
+(defn- cardify
   [dir]
   (let [d (clojure.java.io/file dir)
         ds (filter #(re-matches #"\d\d*\.vcf$" (.getName %)) (file-seq d))]
     (map mapify ds)))
 
-(defn csvify
-  [c]
-  (prn "name" ","
-       "mobile phone" ","
-       "home phone" ","
-       "work phone" ","
-       "e-mail address" ","
-       "home address" ","
-       "note"))
+(def ^{:doc "Map tags to contacts.csv column names."
+       :private true}
+  label<-tag
+  {:name "name"
+   :mobile "mobile phone"
+   :home "home phone"
+   :email "e-mail address"
+   :address "home address"
+   :birthday "birthday"
+   :url "URL"
+   :nickname "nickname"
+   :note "note"
+   :fax "fax"
+   :voice "voice phone"
+   :work "work phone"})
+
+(defn- csvify
+  "Write out the sequence of contact maps in c as a CSV file."
+  [cs]
+  (let [ks (keys label<-tag)
+        heads (map pr-str (map label<-tag ks))
+        fields (fn [m] (map #(clojure.string/join "," (m % "")) ks))
+        strify (fn [m] (map pr-str (fields m)))
+        row (fn [m] (clojure.string/join "," (strify m)))]
+    (println (clojure.string/join "," heads))
+    (doseq [m cs] (println (row m)))))
 
 (defn -main
   [& args]
-  (doseq [arg args] (prn (cardify arg))))
+  (doseq [arg args] (csvify (cardify arg))))
 
 ;; (-main "/Users/tbl/Nokia/contacts")
